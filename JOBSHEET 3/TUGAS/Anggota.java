b4688bbf3989e4ad9beff107cb7303bb9c142a16
Nadyaaurora <nadyaaurora575@gmail.com>
2026-01-06 15:04:20 +0700

    Jobsheet 3

---
+++ JOBSHEET 3/TUGAS/Anggota.java
@@ -0,0 +1,50 @@
+package TUGAS;
+public class Anggota {
+    private String noKTP;
+    private String nama;
+    private int limitPinjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String noKTP, String nama, int limitPinjaman) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getNoKTP() {
+    return noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int uang) {
+        if (jumlahPinjaman + uang > limitPinjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += uang;
+        }
+    }
+
+    public void angsur(int uang) {
+        if (uang >= jumlahPinjaman * 0.1) {
+            jumlahPinjaman -= uang;
+            if (jumlahPinjaman < 0) {
+                jumlahPinjaman = 0;
+            }
+        } else {
+            System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman.");
+        }
+    }
+}
+
